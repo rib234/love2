a930182fd4db15018624c317c3e75daa98fdc245
rib234 <130084940+rib234@users.noreply.github.com>
2023-04-06 22:41:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+// 霸都丶傲天 2019/7/9  https://Github.com/AJLoveChina
+var config = {
+    // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
+    lines: [
+
+        "Hey Chhabu",
+        "I love you 🥰❤️",
+        "Can you give me a kiss ?🥺🥺🥺🥺🥺🥺",
+        "I will hold it on my lips for each and everyday",
+        "i am not gonna do brush too🥺",
+        "please tu bhi le le mujhse",
+        "mekko ek go chahiye please",
+        "achcha thik hai mat de",
+        "pr yaad rkhna tu mekko kissi nhi degi to",
+        "tu kabhi momos nhi kha paaegi",
+        "tu chahe to mere srap se bach skti hai",
+        "aur uske liye teko pta ba kya krna hai 😋 waiting...."
+   
+   
+    ],
+    // 相爱的时间 记住格式不能写错了, 非常重要
+    // 格式一定要和下面这个时间格式一样
+    // 分别是 年-月-日T时时:分分:秒秒
+    //      温馨警告:
+    //          1.时分秒都必须是俩个数字  比如 00:00:00
+    //          2.T不能丢了
+    date: "2017-07-20T20:15:00",
+    // 双方的名字
+    names: ["Neeraj", "Akanksha"]
+}
